35788ba254f72702ea6c6b8ff4581748f1295752
Julia <julia.gray30@gmail.com>
2022-01-26 22:04:52 +0000

    js added

--- assets/style.js
+++ assets/style.js
@@ -1,8 +1,17 @@
+const search = document.getElementById("search")
+
+let city = search.value
 
 
-// on click or enter of search, the user is interpolated into the api web address 
+
+
+// Saves the city the user has input into local storage ----------------------
+let saveValue = () => {
+    localStorage.setItem("City", search.value)
+}
 
 let cityName = "Birmingham"
+
 
 function getApi() {
     let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric`
@@ -23,3 +32,12 @@ getApi()
 // }
 
 // attainApi()
+
+
+
+// To do
+// Turn the local storage into an array that is then displayed under the search 
+// Pull the information from the API into the weather boxes
+// on click or enter of search, the user is interpolated into the api web address 
+// how to get the five day 
+// Get the text input from the search and save it to local storage 

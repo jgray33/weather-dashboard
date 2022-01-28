f532f607771f0a30fa6f398353975aca504f1c3a
Julia <julia.gray30@gmail.com>
2022-01-28 20:13:42 +0000

    icons added

--- assets/style.js
+++ assets/style.js
@@ -9,13 +9,18 @@ let city = search.value
 let currentTime = moment();
 let todayDate = currentTime.format("DD-MM-YY")
 let tomorrowDate = moment().add(1, "days").format("DD-MM-YY")
+let dateAddTwo = moment().add(2, "days").format("DD-MM-YY")
 
-console.log(tomorrowDate)
+console.log(dateAddTwo)
 
 
 
 $("#today").text(todayDate)
 $("#tomorrow").text(tomorrowDate)
+$("#dateAddTwo").text(dateAddTwo)
+// $("0temp").text(data.temp)
+
+$
 
 
 
@@ -25,33 +30,29 @@ let saveValue = () => {
     localStorage.setItem("City", search.value)
 }
 
+let userCity = localStorage.getItem("City")
+console.log(userCity)
+
 let cityName = "Birmingham"
 
-// Add dates into the boxes ---------------------------------------------------
-let loadDates = () => {
 
-}
-
-
-function getApi() {
+// Fetching data from weather API ---------------------------------------
+async function getApi() {
     let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric`
-        fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data.main)
-    })}
+    const response = await fetch(requestUrl)
+    const data = await response.json()
+    $("#0descript").text(data.weather[0].description)
+    $("#0temp").text(data.main.temp)
+    $("#0humid").text(data.main.humidity)
+    $("#0ws").text(data.wind.speed)
+    console.log(data)
+    let iconcode = (data.weather[0].icon)
+    let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
+    $("#wicon").attr("src", iconUrl)
+    }
 
 getApi()
 
-// async function attainApi() {
-//     let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=Birmingham&appid=f7709e138c9db02bf881e5c64600209b&units=metric"
-//     const response = await fetch(requestUrl)
-//     const data = await response.json()
-//     console.log(data)
-// }
-
-// attainApi()
 
 
 

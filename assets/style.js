afa6ddbf44512e9b49fdb006dbaa9ef9ff0f02b5
Julia <julia.gray30@gmail.com>
2022-01-28 21:35:36 +0000

    first day in

--- assets/style.js
+++ assets/style.js
@@ -7,6 +7,9 @@ let city = search.value
 
 // Time ---------------------
 let currentTime = moment();
+let currentTimeUnix = moment().unix()
+console.log(currentTimeUnix)
+
 let todayDate = currentTime.format("DD-MM-YY")
 let tomorrowDate = moment().add(1, "days").format("DD-MM-YY")
 let dateAddTwo = moment().add(2, "days").format("DD-MM-YY")
@@ -33,25 +36,39 @@ let saveValue = () => {
 let userCity = localStorage.getItem("City")
 console.log(userCity)
 
-let cityName = "Birmingham"
+let cityName = "London"
 
+let inputValue = document.querySelector(".inputValue")
 
 // Fetching data from weather API ---------------------------------------
 async function getApi() {
-    let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric`
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
     const response = await fetch(requestUrl)
     const data = await response.json()
-    $("#0descript").text(data.weather[0].description)
-    $("#0temp").text(data.main.temp)
-    $("#0humid").text(data.main.humidity)
-    $("#0ws").text(data.wind.speed)
+    // Today's weather
+    $("#0descript").text(data.list[0].weather[0].description)
+    $("#0temp").text(Math.floor(data.list[0].main.temp))
     console.log(data)
-    let iconcode = (data.weather[0].icon)
+    $("#0humid").text(data.list[0].main.humidity)
+    $("#0ws").text(data.list[0].wind.speed)
+    let iconcode = (data.list[0].weather[0].icon)
     let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
     $("#wicon").attr("src", iconUrl)
-    }
+    // 
 
+
+    }
 getApi()
+
+
+
+
+
+
+
+
+
+
 
 
 

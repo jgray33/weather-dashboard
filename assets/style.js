f6f183dd41c7d6f3603917471a51d9915e8c3d90
Julia <julia.gray30@gmail.com>
2022-01-29 16:48:43 +0000

    functions for retrieving the API data updated into one function

--- assets/style.js
+++ assets/style.js
@@ -3,12 +3,10 @@ const search = document.getElementById("search")
 let city = search.value
 
 
-
-
 // Time ---------------------
 let currentTime = moment();
-let currentTimeUnix = moment().unix()
-console.log(currentTimeUnix)
+let currentTimeUnix = moment()
+
 
 let todayDate = currentTime.format("DD-MM-YY")
 let day = moment().format("dddd")
@@ -18,7 +16,6 @@ let dateAddTwo = moment().add(2, "days").format("DD-MM-YY")
 let dayAddTwo = moment().add(2,"days").format("dddd")
 
 console.log(tomorrowDay)
-
 
 
 $("#today").text(todayDate)
@@ -45,56 +42,56 @@ let cityName = "London"
 let inputValue = document.querySelector(".inputValue")
 
 // Fetching data from weather API ---------------------------------------
-async function getApi() {
-    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
-    const response = await fetch(requestUrl)
-    const data = await response.json()
-    // Today's weather
-    let iconcode = (data.list[0].weather[0].icon)
-    let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
-    $("#wicon").attr("src", iconUrl)
-    $("#0descript").text(data.list[0].weather[0].description)
-    $("#0temp").text(Math.floor(data.list[0].main.temp))
-    console.log(data)
-    $("#0humid").text(data.list[0].main.humidity)
-    $("#0ws").text(data.list[0].wind.speed)
-       }
-getApi()
+// async function getApi() {
+//     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
+//     const response = await fetch(requestUrl)
+//     const data = await response.json()
+//     // Today's weather
+//     let iconcode = (data.list[0].weather[0].icon)
+//     let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
+//     $("#wicon").attr("src", iconUrl)
+//     $("#0descript").text(data.list[0].weather[0].description)
+//     $("#0temp").text(Math.floor(data.list[0].main.temp))
+//     console.log(data)
+//     $("#0humid").text(data.list[0].main.humidity)
+//     $("#0ws").text(data.list[0].wind.speed)
+//        }
+// getApi()
 
-// Day 1 weather
-async function getApi1() {
-    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
-    const response = await fetch(requestUrl)
-    const data = await response.json()
-    let iconcode = (data.list[1].weather[0].icon)
-    let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
-    $("#day1icon").attr("src", iconUrl)
-    $("#1descript").text(data.list[1].weather[0].description)
-    $("#1temp").text(Math.floor(data.list[1].main.temp))
-    console.log(data)
-    $("#1humid").text(data.list[1].main.humidity)
-    $("#1ws").text(data.list[1].wind.speed)
-       }
+// // Day 1 weather
+// async function getApi1() {
+//     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
+//     const response = await fetch(requestUrl)
+//     const data = await response.json()
+//     let iconcode = (data.list[1].weather[0].icon)
+//     let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
+//     $("#day1icon").attr("src", iconUrl)
+//     $("#1descript").text(data.list[1].weather[0].description)
+//     $("#1temp").text(Math.floor(data.list[1].main.temp))
+//     console.log(data)
+//     $("#1humid").text(data.list[1].main.humidity)
+//     $("#1ws").text(data.list[1].wind.speed)
+//        }
 
-getApi1()
+// getApi1()
 
-// Day 2 weather
-async function getApi2() {
-    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
-    const response = await fetch(requestUrl)
-    const data = await response.json()
-    console.log(data.list[2].weather[0].icon)
-    let iconcode = (data.list[2].weather[0].icon)
-    let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
-    $("#day2icon").attr("src", iconUrl)
-    $("#2descript").text(data.list[2].weather[0].description)
-    $("#2temp").text(Math.floor(data.list[2].main.temp))
+// // Day 2 weather
+// async function getApi2() {
+//     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=5`
+//     const response = await fetch(requestUrl)
+//     const data = await response.json()
+//     console.log(data.list[2].weather[0].icon)
+//     let iconcode = (data.list[2].weather[0].icon)
+//     let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
+//     $("#day2icon").attr("src", iconUrl)
+//     $("#2descript").text(data.list[2].weather[0].description)
+//     $("#2temp").text(Math.floor(data.list[2].main.temp))
     
-    $("#2humid").text(data.list[2].main.humidity)
-    $("#2ws").text(data.list[2].wind.speed)
-       }
+//     $("#2humid").text(data.list[2].main.humidity)
+//     $("#2ws").text(data.list[2].wind.speed)
+//        }
 
-getApi2()
+// getApi2()
 
 
 
@@ -115,3 +112,34 @@ getApi2()
 // on click or enter of search, the user is interpolated into the api web address 
 // how to get the five day 
 // Get the text input from the search and save it to local storage 
+
+
+// Render #
+
+async function getApi(index) {
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=f7709e138c9db02bf881e5c64600209b&units=metric&cnt=40`
+    const response = await fetch(requestUrl)
+    const data = await response.json()
+    console.log(data)
+    let iconcode = (data.list[index].weather[0].icon)
+    let iconUrl = `http://openweathermap.org/img/w/${iconcode}.png`
+    $("#wicon").attr("src", iconUrl)
+    $("#0descript").text(data.list[index].weather[0].description)
+    $("#0temp").text(Math.floor(data.list[index].main.temp))
+    $("#0humid").text(data.list[index].main.humidity)
+    console.log(data.list[index])
+    console.log(data.list[index].dt_txt)
+    $("#0ws").text(data.list[index].wind.speed)
+       }
+
+getApi(0)
+getApi(8)
+getApi(16)
+getApi(24)
+getApi(32)
+    // Count is every three hours - we need every eight 
+
+// for (let i = 0; i < 10; i++) {
+//     getApi(i)    
+// }
+
